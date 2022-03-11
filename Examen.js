23b61286ea2b3e414e442a3f9b0700939cd7cd09
oscar <ramirez107322@insuco.edu.mx>
2022-03-11 10:37:02 -0600

    examen

--- Examen.js
+++ Examen.js
@@ -1,1 +1,19 @@
 //pruebla
+//se declara tres variables abc ultima variable que van a sumar dos variables y lo van a multiplicar su vountad
+let A = 17,B= 20,C = 24;
+
+let = A = 17,B= 20,c =24;
+
+console.log( B * C);
+
+letA = 29,B = 30, C =54;
+
+let =A = 29,B = 30,C = 54;
+
+console.log( A * B );
+
+letA = 41,B = 62,C=78
+
+let=A=41,B=62,C=78;
+
+console.log(B * C)
